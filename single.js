fb47666f0f7e68a7da87b93c9cc12f69302d2358
tarak0810 <divibalaji0810@gmail.com>
2025-10-16 15:29:18 +0530

    Update single.js

--- single.js
+++ single.js
@@ -1,70 +1,79 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const slides = document.querySelectorAll('.slider-image');
-  const dots = document.querySelectorAll('.dot');
-  const slider = document.querySelector('.slider');
-  const totalSlides = slides.length;
-  let currentSlide = 0;
-  let interval;
-  let touchStartX = 0;
-  let touchEndX = 0;
+const slides = document.querySelectorAll('.slider-image');
+const dots = document.querySelectorAll('.dot');
+const totalSlides = slides.length;
+let currentSlide = 0;
+let interval;
 
-  function showSlide(index) {
-    slides.forEach((slide, i) => {
-      slide.classList.toggle('active', i === index);
-    });
-    dots.forEach((dot, i) => {
-      dot.classList.toggle('active', i === index);
-    });
-  }
-
-  function nextSlide() {
-    currentSlide = (currentSlide + 1) % totalSlides;
-    showSlide(currentSlide);
-  }
-
-  function previousSlide() {
-    currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
-    showSlide(currentSlide);
-  }
-
-  function startSlider() {
-    interval = setInterval(nextSlide, 1000);
-  }
-
-  dots.forEach((dot, i) => {
-    dot.addEventListener('click', () => {
-      clearInterval(interval);
-      currentSlide = i;
-      showSlide(currentSlide);
-      startSlider();
-    });
+// Function to show specific slide
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.toggle('active', i === index);
   });
+  dots.forEach((dot, i) => {
+    dot.classList.toggle('active', i === index);
+  });
+}
 
-  slider.addEventListener('touchstart', (e) => {
-    touchStartX = e.touches[0].clientX;
-    // console.log('Touch start:', touchStartX);
-  }, false);
-
-  slider.addEventListener('touchend', (e) => {
-    touchEndX = e.changedTouches[0].clientX;
-    // console.log('Touch end:', touchEndX);
-    handleGesture();
-  }, false);
-
-  function handleGesture() {
-    const swipeDistance = touchEndX - touchStartX;
-    const minSwipeDistance = 40; // Minimum distance to consider swipe
-    if (swipeDistance < -minSwipeDistance) {
-      clearInterval(interval);
-      nextSlide();
-      startSlider();
-    } else if (swipeDistance > minSwipeDistance) {
-      clearInterval(interval);
-      previousSlide();
-      startSlider();
-    }
-  }
-
+// Function to go to next slide
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % totalSlides;
   showSlide(currentSlide);
-  startSlider();
+}
+
+// Function to go to previous slide
+function previousSlide() {
+  currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+  showSlide(currentSlide);
+}
+
+// Start automatic slider
+function startSlider() {
+  interval = setInterval(nextSlide, 4000);
+}
+
+// Dot navigation
+dots.forEach((dot, i) => {
+  dot.addEventListener('click', () => {
+    clearInterval(interval);
+    currentSlide = i;
+    showSlide(currentSlide);
+    startSlider();
+  });
 });
+
+// Touch support variables
+const slider = document.querySelector('.slider');
+let touchStartX = 0;
+let touchEndX = 0;
+
+// Handle touch start
+slider.addEventListener('touchstart', (e) => {
+  touchStartX = e.touches[0].clientX;
+});
+
+// Handle touch end
+slider.addEventListener('touchend', (e) => {
+  touchEndX = e.changedTouches[0].clientX;
+  handleGesture();
+});
+
+// Detect swipe direction
+function handleGesture() {
+  const swipeDistance = touchEndX - touchStartX;
+  const minSwipeDistance = 40; // threshold
+  if (swipeDistance < -minSwipeDistance) {
+    // Swipe left
+    clearInterval(interval);
+    nextSlide();
+    startSlider();
+  } else if (swipeDistance > minSwipeDistance) {
+    // Swipe right
+    clearInterval(interval);
+    previousSlide();
+    startSlider();
+  }
+}
+
+// Initialize
+showSlide(currentSlide);
+startSlider();
